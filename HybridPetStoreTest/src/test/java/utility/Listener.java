54aa78cbfc76ea1905c9d0233a42f98b79eaf824
TheStranger22 <soumojitsur07@gmail.com>
2023-06-26 15:45:37 +0530

    utility classes added

---
+++ HybridPetStoreTest/src/test/java/utility/Listener.java
@@ -0,0 +1,19 @@
+package utility;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class Listener {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
