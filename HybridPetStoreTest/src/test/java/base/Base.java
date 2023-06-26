1a43306beed4dadafd574098482314ace948e355
Arunima <Arunima@DESKTOP-V1VCLF5>
2023-06-26 14:52:17 +0530

    Updated Base Class

---
+++ HybridPetStoreTest/src/test/java/base/Base.java
@@ -0,0 +1,32 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+
+public class Base {
+	public WebDriver driver;
+  
+  
+  @BeforeMethod
+  public void setup() {
+	  	WebDriverManager.chromedriver().setup();
+	  	driver = new ChromeDriver();
+		driver.get("https://petstore.octoperf.com/actions/Catalog.action");
+		driver.manage().window().maximize();
+		Reporter.log("Browser Maximized");
+		String OriginalTitle=driver.getTitle();
+		System.out.println("The page title is :"+OriginalTitle);
+		Reporter.log("URL has been launched succesfully");		
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+  }
+
+}
